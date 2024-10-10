27a8fb1cfbc67a068e655509462f2a181607e638
403-ko2 <zaekotu@gmail.com>
2024-10-10 11:15:57 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 //write your code here
+
+const reverseString = (str) => {
+  //   return str.slice(str[str - 1], -1);
+  let result = "";
+  for (i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
+};
+
+console.log(reverseString("hello"));
+
+const reverseZigZagString = (str) => {
+  let result = "";
+  let isUpperCase = false;
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    let char = str[i];
+    if (isUpperCase) {
+      result += char.toUpperCase();
+    } else {
+      result += char.toLowerCase();
+    }
+    isUpperCase = !isUpperCase;
+  }
+  return result;
+};
+
+console.log(reverseZigZagString("Hello"));
